65b703e49ddddbe0539990a7a73bf80c0f19c9a6
Hydrox <139654803+welomenchaina@users.noreply.github.com>
2025-06-20 13:23:08 -0500

    Create InformationPuller.lua

---
+++ InformationPuller.lua
@@ -0,0 +1,8 @@
+local GameId = game.GameId
+local GamePlaceId = game.PlaceId
+local MarketplaceService = game:GetService("MarketplaceService")
+local gameName = MarketplaceService:GetProductInfo(game.PlaceId).Name
+local jobid = game.JobId
+local privowner = game.PrivateServerOwnerId
+local privid = game.PrivateServerid
+
