b177b46110632bc369917726bcd564cf1748c275
Hydrox <139654803+welomenchaina@users.noreply.github.com>
2026-03-05 11:04:43 -0600

    Introduce CustomTime for task wait duration
    Add CustomTime variable with default value.

--- CerberusBizzareLineage.lua
+++ CerberusBizzareLineage.lua
@@ -1,4 +1,12 @@
 local key = getgenv().Key
+local CustomTime = getgenv().CustomTime
+if not CustomTime then
+  CustomTime = 10.6
+end
+
+if key then
+task.wait(CustomTime)
 
 script_key=key;
 loadstring(game:HttpGet("https://www.getcerberus.com/loader.lua"))()
+end
